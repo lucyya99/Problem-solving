0fde3c98db07f081cfccd9f3e64fbf75c56b7563
Yeji Kim <80736490+lucyya99@users.noreply.github.com>
2024-07-18 10:43:17 +0900

    [Silver II] Title: 나무 자르기, Time: 416 ms, Memory: 113920 KB -BaekjoonHub

--- Python/백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ Python/백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,5 +1,9 @@
-N, M = map(int, input().split())
-arr = [int(n) for n in input().split()]
+from sys import stdin
+from collections import Counter
+
+N, M = map(int, stdin.readline().split())
+arr = Counter(map(int, stdin.readline().split()))
+
 # 절단기에 설정할 수 있는 높이 = H => 나무의 최대 높이
 start = 0
 end = max(arr)
@@ -7,8 +11,8 @@ end = max(arr)
 while start <= end:
     mid = start + (end - start) // 2
     # [T T T ... F F F]
-    H = sum(n - mid for n in arr if n - mid > 0)
-    if H >= M:
+    # sum > for (함수 콜링시 Pypi가 더 빠름 / Python3는 내장함수 사용해서 감싸는게 더 빠름)
+    if sum((i - mid)*v for i, v in arr.items() if i - mid > 0) >= M:
         start = mid + 1
         answer = mid
     else:
