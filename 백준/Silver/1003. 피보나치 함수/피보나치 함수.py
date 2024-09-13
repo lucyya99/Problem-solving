0982712c405345a8ce580891d03733a4ade629bd
Yeji Kim <80736490+lucyya99@users.noreply.github.com>
2024-09-13 14:49:43 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -6,25 +6,17 @@ fibo[1] = 1
 
 fibo_0 = [0] * 41
 fibo_0[0] = 1
-fibo_0[2] = 1
-fibo_0[3] = 1
 fibo_1 = [0] * 41
 fibo_1[1] = 1
-fibo_1[2] = 1
-fibo_1[3] = 2
+
 
 def fibonacci(n: int) -> int:
     global fibo, fibo_0, fibo_1
-    if fibo[n] >= 0:
-        return fibo[n]
-    else:
+    if fibo[n] < 0:
         fibo[n] = fibonacci(n-1) + fibonacci(n - 2)
-
-        if n > 3:
-            fibo_0[n] = fibo_0[n - 2] + fibo_0[n - 1]
-            fibo_1[n] = fibo_1[n - 2] + fibo_1[n - 1]
-
-        return fibo[n]
+        fibo_0[n] = fibo_0[n-2] + fibo_0[n-1]
+        fibo_1[n] = fibo_1[n-2] + fibo_1[n-1]
+    return fibo[n]
 
 
 fibonacci(40)
